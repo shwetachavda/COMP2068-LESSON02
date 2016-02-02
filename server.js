6a5b27785ecd9c8c5be81a55250eab8b2be7f0a0
shwetachavda <shwetachavda13@gmail.com>
2016-02-02 12:47:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var http = require('http');
 
-var hostname = '127.0.0.1';
-var port = 8080;
+
+var port =process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port, hostname, function () {
-    console.log("Server running at http://" + hostname + ":" + port);
-});
-//# sourceMappingURL=index.js.map
+}).listen(port);
+//# sourceMappingURL=server.js.map
